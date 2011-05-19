21bb22a39e7242de6f81a8108b5e0d8ec49e3d04
T S <YenTheFirst@gmail.com>
2011-05-19 16:02:38 -0700

    initial commit

---
+++ upload.rb
@@ -0,0 +1,23 @@
+require 'sinatra'
+require 'haml'
+require 'json'
+require 'couchrest'
+require 'base64'
+
+
+
+
+get "/" do
+	haml :upload
+end
+
+post "/upload_file" do
+	@db = CouchRest.database!("http://127.0.0.1:5984/boston_api")
+	f = params[:upload][:file]
+	@db.save_doc({"_attachments" => {
+		f[:filename]=>{
+			"content_type"=>f[:type],
+			"data"=>f[:tempfile].read
+		}
+	}})
+end
