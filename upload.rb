1177cd54487e825b071b39c94103027fe6abfd65
T S <YenTheFirst@gmail.com>
2011-05-31 12:27:28 -0700

    recognize the new fields

--- upload.rb
+++ upload.rb
@@ -5,7 +5,7 @@ require 'couchrest'
 require 'base64'
 
 
-
+FIELDS=%w(title comments data_url organization department name email phone)
 
 get "/" do
 	haml :upload
@@ -14,11 +14,15 @@ end
 post "/upload_file" do
 	@db = CouchRest.database!("http://127.0.0.1:5984/boston_api")
 	f = params[:upload][:file]
-	@db.save_doc({"_attachments" => {
+
+	doc = params[:upload].select {|k,v| FIELDS.include? k}
+	doc["_attachments"]={
 		f[:filename]=>{
 			"content_type"=>f[:type],
 			"data"=>f[:tempfile].read
 		}
-	}})
+	}
+	
+	@db.save_doc(doc)
 	"upload successful"
 end
